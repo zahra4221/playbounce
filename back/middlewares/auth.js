0f8f93d6c0e6775a70b85ebba04197958d13f5cd
zahra4221 <shakilzahra1@gmail.com>
2024-05-18 00:07:28 +0200

    mise en place base de donnée, inscription et connexion

---
+++ back/middlewares/auth.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+const userAuthToken = process.env.ZAHRA_SECRET_TOKEN;
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decodedToken = jwt.verify(token, userAuthToken);
+    const userId = decodedToken.userId;
+    req.auth = {
+      userId: userId,
+    };
+    next();
+  } catch (error) {
+    res.status(401).json({ error });
+  }
+};
