1b4f090e61789c73316e7fb42e7b2a4d14d6c953
zahra4221 <121237096+zahra4221@users.noreply.github.com>
2024-05-25 12:27:12 +0200

    Revert "Adminpage"

--- back/routes/reservationRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const reservationController = require('../controllers/reservationController');
-const auth = require('../middlewares/auth');
-// Route pour réserver un événement
-router.post('/:id', auth, reservationController.reserveMatch);
-
-module.exports = router;
